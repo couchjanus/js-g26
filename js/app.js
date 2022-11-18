2280bfd8f271f8ee68fbb196d1035947be6c9344
couchjanus <couchjanus@gmail.com>
2022-11-18 21:10:26 +0200

    added unit 2

--- js/app.js
+++ js/app.js
@@ -1,62 +1,64 @@
 /**
  * documentations
  */
-
-
-// comment
-
-//
 "use strict"
-// var a = 1;
-// let b = 2; //Numbere
-// const $test = "test";
-// let abc123_qwe$ = 'Hello world';
-// var ADD = `+`; // String
-// // + - * / %
-// {
-//     a = 777;
-//     let c = a + b;
-//     console.log("a + b = ", c);
-// }
+const shoppingCart = document.querySelector('.shopping-cart');
 
-// // $test = "TEST";
-// ADD = "AAAAA";
-// b = 1234;
-// let c = a + b;
-// console.log("a + b = ", c);
-// console.log(abc123_qwe$-ADD);
-// alert(abc123_qwe$-ADD+$test);
+let addToCart = document.querySelector('.add-to-cart');
 
-
-let x = prompt("Enter x =", 0);
-let y = prompt("Enter y =", 0);
-
-let c;
-
-if (isNaN(x) || isNaN(y) ) {
-    c = "x not a Number or y not a number";
-}else{
-    x =  parseInt(x);
-    y =  parseInt(y);
-    let o = prompt("Enter operation: ", '+');
-    
-    if(o == '+'){
-        c = x + parseInt(y);
-   }
-   else if(o == '-'){
-        c = parseInt(x) - parseInt(y);
-   }
-   else if(o == '*'){
-       c = parseInt(x) * parseInt(y);
-   }
-   else if(o == '/'){
-       c = parseInt(x) / parseInt(y);
-   }
-   else{
-       alert("Invaled Operation");
-   }
+function add_to_cart(){
+    console.log("Product added to cart");
+    console.log(shoppingCart.textContent);
+    let val = shoppingCart.textContent;
+    // val = parseInt(val);
+    val = +val;
+    // val = val + 1;
+    // val +=1;
+    ++val;
+    shoppingCart.textContent = val;
 }
 
+// addToCart.onclick = add_to_cart;
 
-alert(c);
+// addToCart.addEventListener('click', add_to_cart);
 
+// addToCart.addEventListener('click', function(){
+//     let val = shoppingCart.textContent;
+//     val = +val;
+//     ++val;
+//     shoppingCart.textContent = val;
+// });
+
+// addToCart.addEventListener('click', () => {
+//     let val = shoppingCart.textContent;
+//     val = +val;
+//     ++val;
+//     shoppingCart.textContent = val;
+// });
+// shoppingCart.textContent = 10;
+// console.log(shoppingCart.textContent);
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    document.querySelector('.navbar-toggler').addEventListener('click', () => document.querySelector('.collapse').classList.toggle('show'));
+
+
+    addToCart.addEventListener('click', () => {
+        let val = shoppingCart.textContent;
+        val = +val;
+        ++val;
+        shoppingCart.textContent = val;
+    });
+
+    document.querySelector('.add-to-wishlist').addEventListener('click', () => {
+        let val = document.querySelector('.wish-list').textContent;
+        val = +val;
+        ++val;
+        document.querySelector('.wish-list').textContent = val;
+        // document.querySelector('.wish-list').style.cssText += 'color: red; font-size:2rem;';
+        document.querySelector('.wish-list').style.color = 'red';
+    });
+    // add-to-wishlist
+});
